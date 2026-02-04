0b4c95592c063becd64d5a2c628f17d3107a025c
Aqua <ollie@ollierobinson.co.uk>
2026-02-04 22:13:38 +0000

    Move to TerraFX DXC as Vortice isn't xplat

--- src/Sprout.Graphics/Utils/ShaderUtils.cs
+++ src/Sprout.Graphics/Utils/ShaderUtils.cs
@@ -1,6 +1,11 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using Silk.NET.SPIRV.Cross;
-using Vortice.Dxc;
-using Result = SharpGen.Runtime.Result;
+using TerraFX.Interop.DirectX;
+using TerraFX.Interop.Windows;
+using static TerraFX.Interop.DirectX.DirectX;
+using static TerraFX.Interop.Windows.CLSID;
+using static TerraFX.Interop.Windows.Windows;
 using SpvResult = Silk.NET.SPIRV.Cross.Result;
 
 namespace Sprout.Graphics.Utils;
@@ -25,11 +30,18 @@ internal static class ShaderUtils
         };
     }
     
-    public static byte[] HlslToSpirv(ShaderStage stage, string hlsl, string entryPoint, string? includeDirectory)
+    public static unsafe byte[] HlslToSpirv(ShaderStage stage, string hlsl, string entryPoint, string? includeDirectory)
     {
-        IDxcUtils utils = Dxc.CreateDxcUtils();
-        IDxcCompiler3 compiler = Dxc.CreateDxcCompiler<IDxcCompiler3>();
+        Guid clsidDxcUtils = CLSID_DxcUtils;
+        Guid clsidDxcCompiler = CLSID_DxcCompiler;
+        
+        IDxcUtils* utils;
+        CheckResult(DxcCreateInstance(&clsidDxcUtils, __uuidof<IDxcUtils>(), (void**) &utils), "Create IDxcUtils");
 
+        IDxcCompiler3* compiler;
+        CheckResult(DxcCreateInstance(&clsidDxcCompiler, __uuidof<IDxcCompiler3>(), (void**) &compiler),
+            "Create IDxcCompiler3");
+        
         string profile = stage switch
         {
             ShaderStage.Vertex => "vs_6_0",
@@ -44,16 +56,50 @@ internal static class ShaderUtils
             args.Add(includeDirectory);
         }
 
-        IDxcCompilerArgs compilerArgs = utils.BuildArguments(null, entryPoint, profile, args.ToArray(), null);
-        IDxcIncludeHandler includeHandler = utils.CreateDefaultIncludeHandler();
+        char* pEntryPoint = (char*) Marshal.StringToHGlobalAnsi(entryPoint);
+        char* pProfile = (char*) Marshal.StringToHGlobalAnsi(profile);
 
-        IDxcResult result = compiler.Compile(hlsl, compilerArgs.Arguments, includeHandler);
-        Result status = result.GetStatus();
+        char** pArgs = stackalloc char*[args.Count];
+        for (int i = 0; i < args.Count; i++)
+            pArgs[i] = (char*) Marshal.StringToHGlobalAnsi(args[i]);
 
-        if (status.Failure)
-            throw new Exception($"Compilation failure: {result.GetErrors()}");
+        IDxcCompilerArgs* compilerArgs;
+        CheckResult(
+            utils->BuildArguments(null, pEntryPoint, pProfile, pArgs, (uint) args.Count, null, 0, &compilerArgs),
+            "Build arguments");
 
-        return result.GetObjectBytecodeArray();
+        IDxcIncludeHandler* includeHandler;
+        CheckResult(utils->CreateDefaultIncludeHandler(&includeHandler), "Create default include handler");
+
+        DxcBuffer buffer = new()
+        {
+            Ptr = (void*) Marshal.StringToHGlobalAnsi(hlsl),
+            Size = (nuint) hlsl.Length
+        };
+
+        IDxcResult* result;
+        CheckResult(
+            compiler->Compile(&buffer, compilerArgs->GetArguments(), compilerArgs->GetCount(), includeHandler,
+                __uuidof<IDxcResult>(), (void**) &result), "Compile");
+
+        HRESULT status;
+        CheckResult(result->GetStatus(&status), "Get status");
+
+        if (status.FAILED)
+        {
+            IDxcBlobEncoding* errorBlob;
+            CheckResult(result->GetErrorBuffer(&errorBlob), "Get error buffer");
+            throw new Exception($"Failed to compile HLSL: {new string((sbyte*) errorBlob->GetBufferPointer())}");
+        }
+
+        IDxcBlob* compiled;
+        CheckResult(result->GetResult(&compiled), "Get result");
+
+        byte[] spirv = new byte[compiled->GetBufferSize()];
+        fixed (byte* pSpirv = spirv)
+            Unsafe.CopyBlock(pSpirv, compiled->GetBufferPointer(), (uint) spirv.Length);
+
+        return spirv;
     }
 
     public static unsafe string SpirvToGLSL(ShaderStage stage, byte[] spirv, string entryPoint)
@@ -94,9 +140,15 @@ internal static class ShaderUtils
         }
     }
 
+    public static void CheckResult(HRESULT result, string operation)
+    {
+        if (result.FAILED)
+            throw new Exception($"DXC operation '{operation}' failed with HRESULT: {result.Value:x8}");
+    }
+
     private static void CheckResult(SpvResult result, string operation)
     {
-        if (result != Silk.NET.SPIRV.Cross.Result.Success)
+        if (result != Result.Success)
             throw new Exception($"SPIRV-Cross operation '{operation}' failed: {result}");
     }
 }

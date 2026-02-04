00d56c1f3f23e4d7e4d0f9b2d68cf31e5c18b9bf
Aqua <ollie@ollierobinson.co.uk>
2026-02-04 20:56:22 +0000

    SpirvToGLSL, TranspileHLSL

--- src/Sprout.Graphics/Utils/ShaderUtils.cs
+++ src/Sprout.Graphics/Utils/ShaderUtils.cs
@@ -1,10 +1,30 @@
-using SharpGen.Runtime;
+using Silk.NET.SPIRV.Cross;
 using Vortice.Dxc;
+using Result = SharpGen.Runtime.Result;
+using SpvResult = Silk.NET.SPIRV.Cross.Result;
 
 namespace Sprout.Graphics.Utils;
 
 internal static class ShaderUtils
 {
+    private static Cross _spirv;
+
+    static ShaderUtils()
+    {
+        _spirv = Cross.GetApi();
+    }
+
+    public static string TranspileHLSL(Backend backend, ShaderStage stage, string hlsl, string entryPoint, string? includeDirectory = null)
+    {
+        byte[] spirv = HlslToSpirv(stage, hlsl, entryPoint, includeDirectory);
+
+        return backend switch
+        {
+            Backend.OpenGL => SpirvToGLSL(stage, spirv, entryPoint),
+            _ => throw new ArgumentOutOfRangeException(nameof(backend), backend, null)
+        };
+    }
+    
     public static byte[] HlslToSpirv(ShaderStage stage, string hlsl, string entryPoint, string? includeDirectory)
     {
         IDxcUtils utils = Dxc.CreateDxcUtils();
@@ -34,5 +54,49 @@ internal static class ShaderUtils
             throw new Exception($"Compilation failure: {result.GetErrors()}");
 
         return result.GetObjectBytecodeArray();
+    }
+
+    public static unsafe string SpirvToGLSL(ShaderStage stage, byte[] spirv, string entryPoint)
+    {
+        Context* context;
+        CheckResult(_spirv.ContextCreate(&context), "Create compiler");
+
+        try
+        {
+            ParsedIr* ir;
+            fixed (byte* pSpirv = spirv)
+            {
+                SpvResult result = _spirv.ContextParseSpirv(context, (uint*) pSpirv, (nuint) spirv.Length, &ir);
+                if (result != SpvResult.Success)
+                    throw new Exception($"Failed to parse Spir-V: {_spirv.ContextGetLastErrorStringS(context)}");
+            }
+
+            Compiler* compiler;
+            CheckResult(
+                _spirv.ContextCreateCompiler(context, Silk.NET.SPIRV.Cross.Backend.Glsl, ir, CaptureMode.TakeOwnership,
+                    &compiler), "Create compiler");
+
+            CompilerOptions* options;
+            CheckResult(_spirv.CompilerCreateCompilerOptions(compiler, &options), "Create compiler options");
+            CheckResult(_spirv.CompilerOptionsSetUint(options, CompilerOption.GlslVersion, 330),
+                "Set compiler version");
+            
+            CheckResult(_spirv.CompilerInstallCompilerOptions(compiler, options), "Install compiler options");
+
+            byte* output;
+            CheckResult(_spirv.CompilerCompile(compiler, &output), "Compile");
+
+            return new string((sbyte*) output);
+        }
+        finally
+        {
+            _spirv.ContextDestroy(context);
+        }
+    }
+
+    private static void CheckResult(SpvResult result, string operation)
+    {
+        if (result != Silk.NET.SPIRV.Cross.Result.Success)
+            throw new Exception($"SPIRV-Cross operation '{operation}' failed: {result}");
     }
 }

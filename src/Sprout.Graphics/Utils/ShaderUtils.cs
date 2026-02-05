bd0864c6b1816e43d3a4d23c3f6819bdfc0a11fd
Aqua <ollie@ollierobinson.co.uk>
2026-02-05 00:41:45 +0000

    HlslToSpirv cleanup resources

--- src/Sprout.Graphics/Utils/ShaderUtils.cs
+++ src/Sprout.Graphics/Utils/ShaderUtils.cs
@@ -37,68 +37,95 @@ internal static class ShaderUtils
         Guid clsidDxcUtils = CLSID_DxcUtils;
         Guid clsidDxcCompiler = CLSID_DxcCompiler;
         
-        IDxcUtils* utils;
-        CheckResult(DxcCreateInstance(&clsidDxcUtils, __uuidof<IDxcUtils>(), (void**) &utils), "Create IDxcUtils");
+        IDxcUtils* utils = null;
+        IDxcCompiler3* compiler = null;
+        IDxcCompilerArgs* compilerArgs = null;
+        IDxcIncludeHandler* includeHandler = null;
+        IDxcResult* result = null;
+        IDxcBlob* compiled = null;
+        IDxcBlobEncoding* errorBlob = null;
 
-        IDxcCompiler3* compiler;
-        CheckResult(DxcCreateInstance(&clsidDxcCompiler, __uuidof<IDxcCompiler3>(), (void**) &compiler),
-            "Create IDxcCompiler3");
-        
-        string profile = stage switch
+        try
         {
-            ShaderStage.Vertex => "vs_6_0",
-            ShaderStage.Pixel => "ps_6_0",
-            _ => throw new ArgumentOutOfRangeException(nameof(stage), stage, null)
-        };
+            CheckResult(DxcCreateInstance(&clsidDxcUtils, __uuidof<IDxcUtils>(), (void**) &utils), "Create IDxcUtils");
 
-        List<string> args = ["-spirv"];
-        if (includeDirectory != null)
-        {
-            args.Add("-I");
-            args.Add(includeDirectory);
+            CheckResult(DxcCreateInstance(&clsidDxcCompiler, __uuidof<IDxcCompiler3>(), (void**) &compiler),
+                "Create IDxcCompiler3");
+
+            string profile = stage switch
+            {
+                ShaderStage.Vertex => "vs_6_0",
+                ShaderStage.Pixel => "ps_6_0",
+                _ => throw new ArgumentOutOfRangeException(nameof(stage), stage, null)
+            };
+
+            List<string> args = ["-spirv"];
+            if (includeDirectory != null)
+            {
+                args.Add("-I");
+                args.Add(includeDirectory);
+            }
+
+            using DxcString pEntryPoint = entryPoint;
+            using DxcString pProfile = profile;
+            using DxcStringArray pArgs = new(args);
+
+            CheckResult(utils->BuildArguments(null, pEntryPoint, pProfile, pArgs, pArgs.Length, null, 0, &compilerArgs),
+                "Build arguments");
+
+            CheckResult(utils->CreateDefaultIncludeHandler(&includeHandler), "Create default include handler");
+
+            DxcBuffer buffer = new()
+            {
+                Ptr = (void*) Marshal.StringToHGlobalAnsi(hlsl),
+                Size = (nuint) hlsl.Length
+            };
+
+            CheckResult(
+                compiler->Compile(&buffer, compilerArgs->GetArguments(), compilerArgs->GetCount(), includeHandler,
+                    __uuidof<IDxcResult>(), (void**) &result), "Compile");
+
+            HRESULT status;
+            CheckResult(result->GetStatus(&status), "Get status");
+
+            if (status.FAILED)
+            {
+                CheckResult(result->GetErrorBuffer(&errorBlob), "Get error buffer");
+                throw new Exception($"Failed to compile HLSL: {new string((sbyte*) errorBlob->GetBufferPointer())}");
+            }
+
+            CheckResult(result->GetResult(&compiled), "Get result");
+
+            byte[] spirv = new byte[compiled->GetBufferSize()];
+            fixed (byte* pSpirv = spirv)
+                Unsafe.CopyBlock(pSpirv, compiled->GetBufferPointer(), (uint) spirv.Length);
+
+            return spirv;
         }
-
-        using DxcString pEntryPoint = entryPoint;
-        using DxcString pProfile = profile;
-        using DxcStringArray pArgs = new(args);
-
-        IDxcCompilerArgs* compilerArgs;
-        CheckResult(
-            utils->BuildArguments(null, pEntryPoint, pProfile, pArgs, pArgs.Length, null, 0, &compilerArgs),
-            "Build arguments");
-
-        IDxcIncludeHandler* includeHandler;
-        CheckResult(utils->CreateDefaultIncludeHandler(&includeHandler), "Create default include handler");
-
-        DxcBuffer buffer = new()
+        finally
         {
-            Ptr = (void*) Marshal.StringToHGlobalAnsi(hlsl),
-            Size = (nuint) hlsl.Length
-        };
+            // Is this a good solution? Who knows! Does it work? Yes!
+            if (errorBlob != null)
+                errorBlob->Release();
 
-        IDxcResult* result;
-        CheckResult(
-            compiler->Compile(&buffer, compilerArgs->GetArguments(), compilerArgs->GetCount(), includeHandler,
-                __uuidof<IDxcResult>(), (void**) &result), "Compile");
+            if (compiled != null)
+                compiled->Release();
 
-        HRESULT status;
-        CheckResult(result->GetStatus(&status), "Get status");
+            if (result != null)
+                result->Release();
+            
+            if (includeHandler != null)
+                includeHandler->Release();
 
-        if (status.FAILED)
-        {
-            IDxcBlobEncoding* errorBlob;
-            CheckResult(result->GetErrorBuffer(&errorBlob), "Get error buffer");
-            throw new Exception($"Failed to compile HLSL: {new string((sbyte*) errorBlob->GetBufferPointer())}");
+            if (compilerArgs != null)
+                compilerArgs->Release();
+            
+            if (compilerArgs != null)
+                compiler->Release();
+            
+            if (utils != null)
+                utils->Release();
         }
-
-        IDxcBlob* compiled;
-        CheckResult(result->GetResult(&compiled), "Get result");
-
-        byte[] spirv = new byte[compiled->GetBufferSize()];
-        fixed (byte* pSpirv = spirv)
-            Unsafe.CopyBlock(pSpirv, compiled->GetBufferPointer(), (uint) spirv.Length);
-
-        return spirv;
     }
 
     public static unsafe string SpirvToGLSL(ShaderStage stage, byte[] spirv, string entryPoint)

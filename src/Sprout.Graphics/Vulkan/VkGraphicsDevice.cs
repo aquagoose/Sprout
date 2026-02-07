8806658587a83eddf20d4afcd69491b6e146670f
Aqua <ollie@ollierobinson.co.uk>
2026-02-07 23:24:57 +0000

    swapchain creation

--- src/Sprout.Graphics/Vulkan/VkGraphicsDevice.cs
+++ src/Sprout.Graphics/Vulkan/VkGraphicsDevice.cs
@@ -1,16 +1,29 @@
+using SDL3;
 using Silk.NET.Vulkan;
+using Silk.NET.Vulkan.Extensions.KHR;
+using Image = Silk.NET.Vulkan.Image;
 
 namespace Sprout.Graphics.Vulkan;
 
 internal sealed unsafe class VkGraphicsDevice : GraphicsDevice
 {
+    private const uint FramesInFlight = 3;
+    
     public override bool IsDisposed { get; protected set; }
 
     private readonly Vk _vk;
+    private readonly KhrSurface _khrSurface;
+    private readonly KhrSwapchain _khrSwapchain;
+    
     private readonly Instance _instance;
+    private readonly SurfaceKHR _surface;
     private readonly PhysicalDevice _physicalDevice;
     private readonly VkHelper.Queues _queues;
     private readonly Device _device;
+
+    private SwapchainKHR _swapchain;
+    private Image[] _swapchainImages;
+    private ImageView[] _swapchainImageViews;
     
     public override Backend Backend => Backend.Vulkan;
 
@@ -18,8 +31,27 @@ internal sealed unsafe class VkGraphicsDevice : GraphicsDevice
     {
         _vk = Vk.GetApi();
         _instance = VkHelper.CreateInstance(_vk, AppDomain.CurrentDomain.FriendlyName);
+        
+        if (!_vk.TryGetInstanceExtension(_instance, out _khrSurface))
+            throw new Exception("Failed to get KhrSurface extension!");
+        
+        if (!SDL.VulkanCreateSurface(sdlWindow, _instance.Handle, IntPtr.Zero, out IntPtr surface))
+            throw new Exception($"Failed to create surface: {SDL.GetError()}");
+        _surface = new SurfaceKHR((ulong) surface);
+        
         _physicalDevice = VkHelper.PickPhysicalDevice(_vk, _instance, out _queues);
         _device = VkHelper.CreateDevice(_vk, _physicalDevice, ref _queues);
+        
+        if (!_vk.TryGetDeviceExtension(_instance, _device, out _khrSwapchain))
+            throw new Exception("Failed to get KhrSwapchain extension!");
+
+        _swapchain = VkHelper.CreateSwapchain(_khrSwapchain, _physicalDevice, _device, in _queues, _surface,
+            _khrSurface, sdlWindow, new SwapchainKHR(), out Format format);
+        _swapchainImages = VkHelper.GetSwapchainImages(_device, _khrSwapchain, _swapchain);
+
+        _swapchainImageViews = new ImageView[_swapchainImages.Length];
+        for (int i = 0; i < _swapchainImages.Length; i++)
+            _swapchainImageViews[i] = VkHelper.CreateImageView(_vk, _device, _swapchainImages[i], format);
     }
     
     public override Shader CreateShader(params ReadOnlySpan<ShaderAttachment> attachments)
@@ -53,7 +85,13 @@ internal sealed unsafe class VkGraphicsDevice : GraphicsDevice
             return;
         IsDisposed = true;
         
+        foreach (ImageView view in _swapchainImageViews)
+            _vk.DestroyImageView(_device, view, null);
+        _khrSwapchain.DestroySwapchain(_device, _swapchain, null);
+        _khrSwapchain.Dispose();
         _vk.DestroyDevice(_device, null);
+        SDL.VulkanDestroySurface(_instance.Handle, (nint) _surface.Handle, IntPtr.Zero);
+        _khrSurface.Dispose();
         _vk.DestroyInstance(_instance, null);
         _vk.Dispose();
     }
--- src/Sprout.Graphics/Vulkan/VkHelper.cs
+++ src/Sprout.Graphics/Vulkan/VkHelper.cs
@@ -2,6 +2,7 @@ using SDL3;
 using Silk.NET.Core;
 using Silk.NET.Vulkan;
 using Silk.NET.Vulkan.Extensions.KHR;
+using Image = Silk.NET.Vulkan.Image;
 
 namespace Sprout.Graphics.Vulkan;
 
@@ -162,6 +163,110 @@ internal static unsafe class VkHelper
         vk.GetDeviceQueue(device, queues.PresentFamily, 0, out queues.Present);
 
         return device;
+    }
+
+    // When passing in a surface, pass the surface to the surface, not the khrSurface.
+    // Pass the KhrSurface to the khrSurface, not the surface, and make sure the surface is in surface, not khrSurface
+    // Got it? Great!
+    public static SwapchainKHR CreateSwapchain(KhrSwapchain khrSwapchain, PhysicalDevice physicalDevice, Device device,
+        ref readonly Queues queues, SurfaceKHR surface, KhrSurface khrSurface, IntPtr sdlWindow,
+        SwapchainKHR oldSwapchain, out Format format)
+    {
+        SurfaceCapabilitiesKHR capabilities;
+        khrSurface.GetPhysicalDeviceSurfaceCapabilities(physicalDevice, surface, &capabilities);
+
+        Extent2D extent = capabilities.CurrentExtent;
+        if (extent.Width == uint.MaxValue || extent.Height == uint.MaxValue)
+        {
+            int w, h;
+            SDL.GetWindowSizeInPixels(sdlWindow, out w, out h);
+            extent.Width = (uint) w;
+            extent.Height = (uint) h;
+        }
+        // Gonna skip the extra size checking here, I'm hoping it won't be necessary.
+
+        // Some impls have a min image count of 1. We want to have at least 2 buffers but respect the min image count
+        // if it wants more.
+        uint numImages = uint.Max(2, capabilities.MinImageCount);
+
+        // TODO: Probably should do additional checking on this. Or even allow the user to specify their own back buffer format.
+        format = Format.B8G8R8A8Unorm;
+        ColorSpaceKHR colorSpace = ColorSpaceKHR.SpaceSrgbNonlinearKhr;
+        PresentModeKHR presentMode = PresentModeKHR.FifoKhr;
+        
+        SwapchainCreateInfoKHR swapchainInfo = new()
+        {
+            SType = StructureType.SwapchainCreateInfoKhr,
+            Surface = surface,
+            OldSwapchain = oldSwapchain,
+            
+            MinImageCount = numImages,
+            ImageExtent = extent,
+            ImageFormat = format,
+            ImageColorSpace = colorSpace,
+            ImageArrayLayers = 1,
+            ImageUsage = ImageUsageFlags.ColorAttachmentBit,
+            ImageSharingMode = SharingMode.Exclusive,
+            
+            PresentMode = presentMode,
+            
+            Clipped = true,
+            CompositeAlpha = CompositeAlphaFlagsKHR.OpaqueBitKhr,
+            PreTransform = SurfaceTransformFlagsKHR.IdentityBitKhr
+        };
+
+        if (queues.GraphicsFamily != queues.PresentFamily)
+        {
+            throw new NotImplementedException(
+                "Currently the Vulkan implementation doesn't support a separate Graphics and Present queue. Please use a different backend.");
+        }
+
+        SwapchainKHR swapchain;
+        khrSwapchain.CreateSwapchain(device, &swapchainInfo, null, &swapchain).Check("Create swapchain");
+
+        return swapchain;
+    }
+
+    public static Image[] GetSwapchainImages(Device device, KhrSwapchain khrSwapchain, SwapchainKHR swapchain)
+    {
+        uint numImages;
+        khrSwapchain.GetSwapchainImages(device, swapchain, &numImages, null);
+        Image[] images = new Image[numImages];
+        fixed (Image* pImages = images)
+            khrSwapchain.GetSwapchainImages(device, swapchain, &numImages, pImages);
+
+        return images;
+    }
+
+    public static ImageView CreateImageView(Vk vk, Device device, Image image, Format format)
+    {
+        ImageViewCreateInfo imageViewInfo = new()
+        {
+            SType = StructureType.ImageViewCreateInfo,
+            Image = image,
+            Format = format,
+            ViewType = ImageViewType.Type2D,
+            Components = new ComponentMapping
+            {
+                R = ComponentSwizzle.Identity,
+                G = ComponentSwizzle.Identity,
+                B = ComponentSwizzle.Identity,
+                A = ComponentSwizzle.Identity
+            },
+            SubresourceRange = new ImageSubresourceRange
+            {
+                AspectMask = ImageAspectFlags.ColorBit,
+                BaseArrayLayer = 0,
+                LayerCount = 1,
+                BaseMipLevel = 0,
+                LevelCount = 1
+            }
+        };
+
+        ImageView view;
+        vk.CreateImageView(device, &imageViewInfo, null, &view).Check("Create image view");
+
+        return view;
     }
 
     public struct Queues

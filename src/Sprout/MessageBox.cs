c65289a45b626233945726fd6882bf106f05dc5c
Aqua <ollie@ollierobinson.co.uk>
2026-03-14 16:40:55 +0000

    simple mesage box

---
+++ src/Sprout/MessageBox.cs
@@ -0,0 +1,26 @@
+﻿using SDL3;
+
+namespace Sprout;
+
+public static class MessageBox
+{
+    public static void Show(Type type, string title, string message)
+    {
+        SDL.MessageBoxFlags flags = type switch
+        {
+            Type.Info => SDL.MessageBoxFlags.Information,
+            Type.Warning => SDL.MessageBoxFlags.Warning,
+            Type.Error => SDL.MessageBoxFlags.Error,
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+
+        SDL.ShowSimpleMessageBox(flags, title, message, IntPtr.Zero);
+    }
+
+    public enum Type
+    {
+        Info,
+        Warning,
+        Error
+    }
+}
